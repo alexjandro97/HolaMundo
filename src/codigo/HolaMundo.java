c9deddfeb930b0c0442c2a9f33181ce38f405981
alex <alex@alexlinuxmint>
2017-02-19 13:43:23 +0100

    Estoy empezando el Hola Mundo

---
+++ src/codigo/HolaMundo.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package codigo;
+
+/**
+ *
+ * @author alex
+ */
+public class HolaMundo {
+    //hola mundo en java
+   // System.out.println("Hola Mundo!!!");
+}
